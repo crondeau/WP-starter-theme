64b5c1da0a424a34d4f4641f7485f85acc95abf5
Christine Rondeau <christine@bluelimemedia.com>
2015-01-02 17:21:34 -0800

    Updated the archive with the new the_archive_title() function.

--- archive.php
+++ archive.php
@@ -14,33 +14,7 @@ get_header(); ?>
 		
 			<?php if (have_posts()) : ?>
 	
-				<?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
-		
-				<?php if (is_category()) { ?>				
-					<h1><?php echo single_cat_title(); ?></h1>
-			
-				<?php } elseif( is_tag() ) { ?>
-					<h1>Posts Tagged: <?php single_tag_title(); ?></h1>
-			
-				<?php } elseif (is_day()) { ?>
-					<h1>Archive for <?php echo get_the_date(); ?></h1>
-			
-				<?php } elseif (is_month()) { ?>
-					<h1>Archive for <?php echo get_the_date( _x( 'F Y', 'monthly archives date format', 'blm_basic' ) ) ?></h1>
-			
-				<?php } elseif (is_year()) { ?>
-					<h1>Archive for <?php echo get_the_date( _x( 'Y', 'yearly archives date format', 'blm_basic' ) ) ?></h1>
-			
-				<?php } elseif (is_search()) { ?>
-					<h1>Search Results</h1>
-			
-				<?php } elseif (is_author()) { ?>
-					<h1>Author Archive</h1>
-			
-				<?php } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
-					<h1>Blog Archives</h1>
-			
-				<?php } ?>
+				<h1><?php the_archive_title(); ?></h1>
 	
 			<?php while (have_posts()) : the_post(); ?>
 			
