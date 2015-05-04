36bddcb899acaec4048431d87d2da38625991542
Christine Rondeau <christine@bluelimemedia.com>
2015-05-04 12:49:31 -0700

    Removed the extra article opening. How did that get in there?

--- content-page.php
+++ content-page.php
@@ -9,13 +9,12 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<header class="entry-header">
-			<h1 class="entry-title"><?php the_title(); ?></h1>
-		</header><!-- .entry-header -->
+	<header class="entry-header">
+		<h1 class="entry-title"><?php the_title(); ?></h1>
+	</header><!-- .entry-header -->
 
-		<div class="entry-content">
-			<?php the_content(); ?>
-		</div><!-- .entry-content -->
+	<div class="entry-content">
+		<?php the_content(); ?>
+	</div><!-- .entry-content -->
 	
 </article><!-- #post-## -->
