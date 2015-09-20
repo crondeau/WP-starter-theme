67f94db9b6a7ba0f37949b682146ffc57f37e063
Giorgio Riccardi <griccardi@my.bcit.ca>
2015-09-19 17:48:19 -0700

    I tried to get rid of the @import in the style.css [Using CSS @import in an external stylesheet can add additional delays during the loading of a web page] Including the stylesheet directory uri it works fine, though I did not test it yet using a child theme.
    Hope it helps
    G.

--- functions.php
+++ functions.php
@@ -66,8 +66,10 @@ require get_template_directory() . '/inc/widgets.php';
  */
 function blm_basic_scripts() {
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
+
+	wp_enqueue_style( 'include', get_stylesheet_directory_uri() . '/css/style.css' );
 	
-	wp_enqueue_style('blm_googleFonts', '//fonts.googleapis.com/css?family=Droid+Serif' );
+	wp_enqueue_style( 'blm_googleFonts', '//fonts.googleapis.com/css?family=Droid+Serif' );
 
 	wp_enqueue_script( 'blm_navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 	
