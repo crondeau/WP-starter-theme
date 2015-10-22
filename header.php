c3abbddcd3875632591a3b08ee7eefdedbbae895
Christine Rondeau <christine@bluelimemedia.com>
2015-10-22 15:48:35 -0700

    Updated the menu toggle. Replaced the h1 with a button.

--- header.php
+++ header.php
@@ -13,14 +13,12 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<?php get_template_part( 'inc/favicons' ); ?>
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 	<![endif]-->
 	
 	<?php wp_head(); ?>
-	
-	<?php get_template_part( 'inc/favicons' ); ?>
-	
 </head>
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">	
@@ -35,7 +33,7 @@
 			</div>
 		
 			<nav id="site-navigation" class="site-navigation main-navigation right-block" role="navigation">
-				<h1 class="menu-toggle"><a href="#"><?php _e( 'Menu', 'blm_basic' ); ?></a></h1>
+				<button class="menu-toggle"><?php _e( 'Menu', 'blm_basic' ); ?></button>
 				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 			</nav>	
 
