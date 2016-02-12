ee395036484d43a703e20d9cee289021cf52c2d9
Christine Rondeau <christine@bluelimemedia.com>
2016-02-12 12:38:58 -0800

    Minor update the 404 css and updated the 404 divs.

--- 404.php
+++ 404.php
@@ -7,11 +7,12 @@
 
 get_header(); ?>
 
-<div id="main" class="row">
+<section class="site-main row">
 	<div class="container">
 
-			<section id="fourofour" class="fourofour-content">
-				
+		<div id="primary" class="fourofour-content">
+			<main id="main" class="site-main" role="main">
+
 				<header class="page-header">
 					<h1 class="page-title">Page not found.</h1>
 				</header><!-- .page-header -->	
@@ -19,9 +20,9 @@ get_header(); ?>
 				<p>We've recently made changes to our website and the page you are looking for might have been deleted or moved. Please <a href="<?php echo home_url(); ?>">visit our home page instead</a>.</p>
 				<p>Sorry for the inconvenience.</p>	
 					
-			</section>
+			</div><!-- .container -->
+		</main><!-- #main -->
 
-	</div><!-- .container -->
-</div><!-- #main -->
-
+	</div>
+</section>
 <?php get_footer(); ?>
