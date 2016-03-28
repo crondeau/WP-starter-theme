b0441af676ac0aa7977aba2c5b4fc09d1d2f93a0
Christine Rondeau <christine@bluelimemedia.com>
2016-03-28 12:03:16 -0700

    Added conditional to remove h1 from pages other than front-page and home.

--- header.php
+++ header.php
@@ -28,8 +28,13 @@
 		<div class="container header-wrapper">	
 		
 			<div id="branding" class="site-branding">
+			<?php if ( is_front_page() && is_home() ) : ?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
 				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			<?php else: ?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></p>
+				<p class="site-description"><?php bloginfo( 'description' ); ?></p>
+			<?php endif; ?>
 			</div>
 		
 			<nav id="site-navigation" class="site-navigation main-navigation" role="navigation">
