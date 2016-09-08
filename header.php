4ae462f0021ef442ccd249f30972e68a4e9a00af
Christine Rondeau <christine@bluelimemedia.com>
2016-09-08 14:44:14 -0700

    Updated header based on underscores.

--- header.php
+++ header.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * The Header for our theme.
+ * The header for our theme.
  *
- * Displays all of the <head> section, header and top navigation areas
+  * This is the template that displays all of the <head> section and everything up until <div id="content">
  *
  * @package blm_basic
  */
@@ -12,7 +12,6 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php get_template_part( 'inc/favicons' ); ?>
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
@@ -21,7 +20,7 @@
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">	
+<div id="page" class="site">	
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'blm_basic' ); ?></a>
 
 	<header id="masthead" class="site-header row" role="banner">	
@@ -38,7 +37,7 @@
 			</div>
 		
 			<nav id="site-navigation" class="site-navigation main-navigation" role="navigation">
-				<button class="menu-toggle"><?php _e( 'Menu', 'blm_basic' ); ?></button>
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'blm_basic' ); ?></button>
 				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 			</nav>	
 
