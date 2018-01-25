e96a60b39afc0733703b8ac7492f9d79de7748cf
Christine Rondeau <christine@bluelimemedia.com>
2018-01-24 17:07:04 -0800

    Fixing header conditional

--- header.php
+++ header.php
@@ -27,7 +27,7 @@
 		<div class="container header-wrapper">	
 		
 			<div id="branding" class="site-branding">
-			<?php if ( is_front_page() && is_home() ) : ?>
+			<?php if ( is_front_page() || is_home() ) : ?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
 				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 			<?php else: ?>
