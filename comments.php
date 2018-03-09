20ef4f71dfdea693b18e5e4fc083669cb725433a
Christine Rondeau <christine@bluelimemedia.com>
2018-03-08 16:38:30 -0800

    Updated the comment classes

--- comments.php
+++ comments.php
@@ -18,12 +18,12 @@ if ( post_password_required() ) {
 }
 ?>
 
-<div id="comments" class="comments-area">
+<div id="comments" class="comments__area">
 
 	<?php
 	// You can start editing here -- including this comment!
 	if ( have_comments() ) : ?>
-		<h3 class="comments-title">
+		<h3 class="title__comments">
 			<?php
 				printf( esc_html( _nx( 'One comment on &ldquo;%2$s&rdquo;', '%1$s comments on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'blm_basic' ) ),
 					number_format_i18n( get_comments_number() ),
@@ -31,7 +31,7 @@ if ( post_password_required() ) {
 			?>
 		</h3>
 
-		<ol class="comment-list">
+		<ol class="comment__list">
 			<?php
 				wp_list_comments( array(
 					'style'      	=> 'ol',
@@ -39,12 +39,12 @@ if ( post_password_required() ) {
 					'avatar_size' 	=> 56,
 				) );
 			?>
-		</ol><!-- .comment-list -->
+		</ol><!-- .comment__list -->
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
-		<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
+		<nav id="comment-nav-below" class="navigation comment__nav" role="navigation">
 			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'blm_basic' ); ?></h2>
-			<div class="nav-links">
+			<div class="comment__nav--links">
 				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'blm_basic' ) ); ?></div>
 				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'blm_basic' ) ); ?></div>
 
