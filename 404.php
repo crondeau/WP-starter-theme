9bc9a4142b95691f0db36a9b9bd8c54bea39475e
Christine Rondeau <christine@bluelimemedia.com>
2019-12-19 15:54:59 -0800

    Updated version number

--- 404.php
+++ 404.php
@@ -15,7 +15,6 @@ get_header(); ?>
 		<p>We've recently made changes to our website and the page you are looking for might have been deleted or moved. Please <a href="<?php echo home_url(); ?>">visit our home page instead</a>.</p>
 		<p>Sorry for the inconvenience.</p>	
 
-		
 	</div>
 </main>
 <?php get_footer(); ?>
